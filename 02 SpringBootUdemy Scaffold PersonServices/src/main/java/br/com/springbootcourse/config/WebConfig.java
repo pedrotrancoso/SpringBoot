3a58c7ba1b3fc8bda93dbdaabde1590a1b6780b8
Pedro Zaluar <pedrotrancosoz@gmail.com>
2020-10-16 20:04:21 -0300

    Implemntação do Cors

--- 02 SpringBootUdemy Scaffold PersonServices/src/main/java/br/com/springbootcourse/config/WebConfig.java
+++ 02 SpringBootUdemy Scaffold PersonServices/src/main/java/br/com/springbootcourse/config/WebConfig.java
@@ -2,10 +2,12 @@ package br.com.springbootcourse.config;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.springbootcourse.converter.YamlJakson2HttpMessageConverter;
@@ -18,19 +20,24 @@ public class WebConfig implements WebMvcConfigurer{
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJakson2HttpMessageConverter());
 	}
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	};
 	
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
 		
 		//Via EXTENSION - http://localhost:8080/api/v1/person.xml
-		/*configurer.favorParameter(false)
+		configurer.favorParameter(false)
 				.ignoreAcceptHeader(false)
 				.defaultContentType(MediaType.APPLICATION_JSON)
 				.mediaType("json", MediaType.APPLICATION_JSON)
-				.mediaType("xml", MediaType.APPLICATION_XML);*/
+				.mediaType("xml", MediaType.APPLICATION_XML);
 		
 		//Via QUERY PARAM - http://localhost:8080/api/v1/person?mediaType=xml
-		configurer.favorPathExtension(false)
+		/*configurer.favorPathExtension(false)
 			.favorParameter(true)
 			.parameterName("mediaType")
 			.ignoreAcceptHeader(true)
@@ -38,7 +45,7 @@ public class WebConfig implements WebMvcConfigurer{
 			.defaultContentType(MediaType.APPLICATION_JSON)
 			.mediaType("json", MediaType.APPLICATION_JSON)
 			.mediaType("xml", MediaType.APPLICATION_XML)
-			.mediaType("x-yaml", MEDIA_TYPE_YAML);
+			.mediaType("x-yaml", MEDIA_TYPE_YAML);*/
 		
 		//Via HEADER
 		/*configurer.favorPathExtension(false)
